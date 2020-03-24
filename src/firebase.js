39c7e2d0607d9091214de327a535642a041c2eab
Deyvi Jara <djara@electrocenterpiura.com>
2020-03-24 18:58:07 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ require("firebase/firestore");
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAfs0xM7Gb-ceu_B4E2GPkcWET-m5s17V0",
+  apiKey: "**********************",
   authDomain: "crud-task-8ebfa.firebaseapp.com",
   databaseURL: "https://crud-task-8ebfa.firebaseio.com",
   projectId: "crud-task-8ebfa",
   storageBucket: "crud-task-8ebfa.appspot.com",
-  messagingSenderId: "1098389120188",
-  appId: "1:1098389120188:web:49373271ed46ba2def1cb9"
+  messagingSenderId: "***************",
+  appId: "******************"
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
